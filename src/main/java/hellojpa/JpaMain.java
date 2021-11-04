90e51b45ee9033f96b082fed614a4e00c757b4f0
sdardew <sdardew@gmail.com>
2021-11-04 19:46:23 +0900

    Apply .gitignore

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
@@ -11,8 +12,20 @@ public class JpaMain {
 
         EntityManager em = emf.createEntityManager();
 
-        em.close();
-        emf.close();
+        // 트랜잭션
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
 
+        Member member = new Member();
+
+        member.setId(1L);
+        member.setName("hello1");
+        em.persist(member); // Member 저장
+
+        tx.commit();
+
+        em.close();
+
+        emf.close();
     }
 }
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,2 +1,29 @@
-package hellojpa;public class Member {
+package hellojpa;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Member") // 테이블 이름 설정
+public class Member {
+    @Id
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
